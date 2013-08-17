c2f6ca15b03b30513960e05df77ca56165f5f3d8
DieBunny <silva_avotina@inbox.lv>
2013-08-17 23:14:23 +0300

    project template

---
+++ Imperium-Infinitum/src/nich/Package/StartPage.java
@@ -0,0 +1,33 @@
+package nich.Package;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.content.Intent;
+import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+
+public class StartPage extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_start_page);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.start_page, menu);
+		return true;
+	}
+	
+	public void NavigateToMain(View v)
+	{
+		Button button = (Button) findViewById(R.id.StartBtnId);
+		Intent intent = new Intent(button.getContext(), MainPage.class);
+		startActivity(intent);
+		
+	}
+
+}
