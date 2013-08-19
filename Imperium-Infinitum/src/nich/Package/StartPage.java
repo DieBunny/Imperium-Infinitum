f67d2c8617ee2c591590d276908620db76178dfa
DieBunny <silva_avotina@inbox.lv>
2013-08-19 19:32:02 +0300

    LinearLayout vertical orientation, max length of player name

--- Imperium-Infinitum/src/nich/Package/StartPage.java
+++ Imperium-Infinitum/src/nich/Package/StartPage.java
@@ -1,5 +1,6 @@
 package nich.Package;
 
+import android.R.color;
 import android.os.Bundle;
 import android.app.Activity;
 import android.app.AlertDialog;
@@ -7,10 +8,14 @@ import android.app.AlertDialog.Builder;
 import android.app.Dialog;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.graphics.Color;
+import android.text.InputFilter;
 import android.view.Menu;
 import android.view.View;
+import android.view.ViewGroup.LayoutParams;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.HorizontalScrollView;
 import android.widget.LinearLayout;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
@@ -47,18 +52,23 @@ public class StartPage extends Activity {
 		Builder dialog = new AlertDialog.Builder(this);
 		dialog.setTitle("Set player names");
 		LinearLayout layout = new LinearLayout(this);
+		layout.setOrientation(LinearLayout.VERTICAL);
 		
 		for (int i=0; i<playerCount; i++)
 		{
 		TextView label = new TextView(layout.getContext());
-		label.setText(i+1 + ". Player name:");
+		//label.setLayoutParams(new LayoutParams(100, LayoutParams.MATCH_PARENT));
+		label.setText(i+1 + ". Player:");
+		InputFilter[] filters = new InputFilter[1];
+		filters[0] = new InputFilter.LengthFilter(12);
 		EditText textfield = new EditText(layout.getContext());
+		//textfield.setLayoutParams(new LayoutParams(100, LayoutParams.MATCH_PARENT));
+		textfield.setFilters(filters);
 		layout.addView(label);
 		layout.addView(textfield);
 		}
 		
 		dialog.setView(layout).setPositiveButton("GO", new DialogInterface.OnClickListener() {
-			
 			@Override
 			public void onClick(DialogInterface dialog, int which) {
 				Intent intent = new Intent(button.getContext(), MainPage.class);
@@ -74,7 +84,6 @@ public class StartPage extends Activity {
 		});
 
 		dialog.show();
-		
 		
 	}
 
